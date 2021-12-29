3694e72fbdf1316486ef6e560f2865d057e50358
Suyunbek <uzcointg@gmail.com>
2021-12-29 12:46:38 +0500

    added comment avatar & update database

--- pages/main.php
+++ pages/main.php
@@ -437,26 +437,17 @@ $sizeComments = count($comments);
                     <h2 class="title comments_title">What <span>my clients</span> Say ?</h2>
                     <div class="comments_content">
                     <?if(count($comments) > 0):?>
-                        <? for ($i=count($comments)-1; $i >= 0; $i--): ?>
+                        <? for ($i=count($comments)-1; $i >= count($comments)-7; $i--): ?>
                         <div class="comment" data-aos="flip-right" data-aos-duration="1000" data-aos-delay="300">
                             <div class="comment_img_blog">
-                                <a href="./?route=profile"><img src="<?= $myProfile['avatar']?>" alt="" class="comment_img" title="View profile"></a>
+                                <a href="./?route=profile"><img src="" alt="" class="comment_img" title="View profile"></a>
                             </div>
                             <div class="comment_body">
                                 <div class="comment_item">
                                     <p class="comment_text"><?= $comments[$i]['comment']?></p>
-                                    <form action="../components/edit-comment.php" method="post" class="comment_edit_form" style="display: none;">
-                                        <textarea name="comment" class="comment_area" value="<?= $comments[$i]['comment']?>" placeholder="Message" required></textarea>
-                                        <input type="hidden" name="id" value="<?= $comments[$i]['id']?>">
-                                        <input type="hidden" name="username" value="<?= $comments[$i]['username']?>">
-                                        <div class="edit_comment_buttons">
-                                            <button class="btn edit_comment_button edit_comment_btn" type="submit">Save</button>
-                                            <button class="btn edit_comment_button cancel_comment_btn" type="button">Cancel</button>
-                                        </div>
-                                    </form>
-                                    <div class="comment_functions" style="display: <?= $isMyProfile ? 'flex' : 'none'?>;">
+                                    <div class="comment_functions" style="display: none;">
                                         <a href="#" class="comment_function" id="edit" title="Edit comment"><i class="fas fa-pen"></i></a href="#">
-                                        <a href="./?route=profile&del=<?= $comments[$i]['id']?>" class="comment_function" id="delete" title="Delete comment"><i class="fas fa-trash"></i></a href="#">
+                                        <a href="#" class="comment_function" id="delete" title="Delete comment"><i class="fas fa-trash"></i></a href="#">
                                     </div>
                                 </div>
                                 <div class="comment_footer">
--- portfolio_2.sql
+++ portfolio_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 27, 2021 at 12:14 PM
+-- Generation Time: Dec 29, 2021 at 10:43 AM
 -- Server version: 10.3.13-MariaDB-log
 -- PHP Version: 7.1.32
 
@@ -21,6 +21,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `portfolio_2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `comment` varchar(300) NOT NULL,
+  `avatar` varchar(200) NOT NULL,
+  `date` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `username`, `comment`, `avatar`, `date`) VALUES
+(1, 'fdgdf', 'gdfgd', '', 'fgsdfgsdfg'),
+(2, 'test', 'sdfdsfdsf', '', '2021-12-27 19:56'),
+(3, 'test', 'tyt ytr ydt 45645 6rtygf hgfhfghg546456', '', '2021-12-27 21:05'),
+(4, 'test', 'fsdfdsfg gdfg df gdfgdfgdfgwe6546y45 5654 dgy r645 g dfg54645645645', '', '2021-12-27 21:10'),
+(8, 'test', 'uigt uhgdfugoidgdsfuyg dfoupghosdfighisdofgh86y dfoiuhgodfghoifdg08th gsdfgsdfg', '', '2021-12-28 20:36'),
+(9, 'fdgdf', 'gdfgd', '', 'fgsdfgsdfg'),
+(10, 'test', 'sdfdsfdsf', '', '2021-12-27 19:56'),
+(11, 'test', 'tyt ytr ydt 45645 6rtygf hgfhfghg546456', '', '2021-12-27 21:05'),
+(12, 'test', 'tyt ytr ydt 45645 6rtygf hgfhfghg546456', '', '2021-12-27 21:05'),
+(13, 'test', 'uigt uhgdfugoidgdsfuyg dfoupghosdfighisdofgh86y dfoiuhgodfghoifdg08th gsdfgsdfg', '', '2021-12-28 20:36');
 
 -- --------------------------------------------------------
 
@@ -51,11 +81,18 @@ INSERT INTO `users` (`id`, `name`, `username`, `email`, `phone`, `password`, `bi
 (4, 'fsdf', 'sdfsd', 'sdfsdf@fsdf.fdgfd', '45345', '$2y$10$YDdeeGPrY0USNX.gMdThLOXTS5OucF8sxeARug7j8m.qpDjoyqRbu', 'sdfgsdfgdsfg', '../img/avatar/no-photo.jpg', '2021-12-26 14:50:12'),
 (5, 'fsdf', 'sdfsdsad', 'sdfsdf@fsdf.fdgfdasd', '45345', '$2y$10$GfbC3j4GjFpHsWnYXdHzSuGA0Jvze9IYdnupzK4e0tdokMDp4vFEy', 'sdfgsdfgdsfg', '../img/avatar/sdfsdsad.jpg', '2021-12-26 14:50:59'),
 (6, 'gdfgdfg', '653reter', 'dfdssdf@fdgfd.gfh', '5345345', '$2y$10$VNHOhsTYfUmtyvaiiSxSEuMY7UFO5vellvDOQUzKGI2m2CGadS1uO', 'fgfdgfhfgh', '../img/avatar/653reter.png', '2021-12-26 18:57:40'),
-(7, 'sgdfgfgfd', 'asd', 'fdgdfg@gh.gh', '56456', '$2y$10$Q2BwUNa2OiAx9J2LOhGBRuchNpSg.8RmYsjXHLS8OJ8.0x8a6xRUa', 'dfgdsfgsd', '../img/avatar/asd.jpg', '2021-12-26 19:24:55');
+(7, 'sgdfgfgfd', 'asd', 'fdgdfg@gh.gh', '56456', '$2y$10$Q2BwUNa2OiAx9J2LOhGBRuchNpSg.8RmYsjXHLS8OJ8.0x8a6xRUa', 'dfgdsfgsd', '../img/avatar/asd.jpg', '2021-12-26 19:24:55'),
+(8, 'dfgsdgfa', 'test', 'fd@ffs', '4565465', '$2y$10$qdG/Ft2rOOaqeRCFYMBmPOZMg70fq8u/fc64p0TXzN2keSIlVGUB6', 'hgfhdfgh', '../img/avatar/test.jpg', '2021-12-27 19:50:03');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -70,10 +107,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
